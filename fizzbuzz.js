5c21d71ed2941f221678011a0ece801a9db561c5
Cameron Grieder <cjgrieder@gmail.com>
2021-12-02 07:38:08 -0700

    removed comment

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,19 +1,19 @@
-// let z  = 1
-// while (z < 51) {
-// if (z % 5 === 0 && z % 3 === 0) {
-//   console.log (`fizz buzz`)
-// }
-// else if (z % 5 === 0) {
-//   console.log (`buzz`)
-// }
-// else if(z % 3 === 0){
-// console.log (`fizz`)
-// }
-// else {
-//   console.log (z)
-// }
-//   z++
-// }
+let z  = 1
+while (z < 51) {
+if (z % 5 === 0 && z % 3 === 0) {
+  console.log (`fizz buzz`)
+}
+else if (z % 5 === 0) {
+  console.log (`buzz`)
+}
+else if(z % 3 === 0){
+console.log (`fizz`)
+}
+else {
+  console.log (z)
+}
+  z++
+}
 
 
 for(let num = 1; num <= 15; num++){
